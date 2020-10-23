f324cea33442a853542775943c9cb63758062a4f
Miguel Eduardo Aguilar Machon <miguelmachon@outlook.com>
2020-10-23 14:37:15 -0600

    Se crearon las Entities / Contry, Departament y City

---
+++ OnSale.Common/Data/Entities/Departament.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OnSale.Common.Data.Entities
+{
+    class Departament
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Departament")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+
+        public int Name { get; set; }
+
+
+    }
+}
